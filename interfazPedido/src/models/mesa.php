0f8ab88cc22d1907882165fbedb91276d64bfd06
09LuisDc2001 <109187049+09LuisDc2001@users.noreply.github.com>
2024-06-11 23:10:56 -0500

    Add files via upload

--- interfazPedido/src/models/mesa.php
+++ interfazPedido/src/models/mesa.php
@@ -10,6 +10,9 @@ class Mesa {
     public $Codqr_id;
     public $Estado_Mesa_id;
     public $Codqr_idCodqr;
+    public $Llamado;
+
+    public $Fechallamado;
 
     public function __construct($db) {
         $this->conn = $db;
@@ -64,6 +67,63 @@ class Mesa {
         }
         return false;
     }
+
+    public function actualizarLlamado() {
+        // Obtener el valor actual de Llamado
+        $query = "SELECT Llamado FROM " . $this->table_name . " WHERE idMesa = :idMesa";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":idMesa", $this->idMesa);
+        $stmt->execute();
+        $currentEstado = $stmt->fetch(PDO::FETCH_ASSOC)['Llamado'];
+    
+        // Alternar el valor de Llamado
+        $newLlamdo = 1;  // Se puede ajustar la lógica si deseas alternar entre 0 y 1
+    
+        // Obtener la fecha y hora actual y restar 7 horas
+        $fechallamado = new DateTime();
+        $fechallamado->modify('-7 hours');
+        $fechallamado = $fechallamado->format('Y-m-d H:i:s');
+    
+        // Actualizar el valor de Llamado y Fechallamado
+        $query = "UPDATE " . $this->table_name . " SET Llamado = :Llamado, Fechallamado = :Fechallamado WHERE idMesa = :idMesa";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":Llamado", $newLlamdo);
+        $stmt->bindParam(":Fechallamado", $fechallamado);
+        $stmt->bindParam(":idMesa", $this->idMesa);
+    
+        if ($stmt->execute()) {
+            echo "yei";
+            return true;
+        }
+        return false;
+    }
+    
+    
+    public function actualizarLlamado2() {
+
+
+        $query = "SELECT Llamado FROM " . $this->table_name . " WHERE idMesa = :idMesa";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":idMesa", $this->idMesa);
+        $stmt->execute();
+        $currentEstado = $stmt->fetch(PDO::FETCH_ASSOC)['Llamado'];
+
+        // Alternar el valor de Estado
+        $newLlamdo = 0;
+
+        // Actualizar el valor de Estado
+        $query = "UPDATE " . $this->table_name . " SET Llamado = :Llamado WHERE idMesa = :idMesa";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":Llamado", $newLlamdo);
+        $stmt->bindParam(":idMesa", $this->idMesa);
+
+        if ($stmt->execute()) {
+            echo ("yei");
+            return true;
+        }
+        return false;
+    }
+    
 
     // Método para eliminar una mesa
     public function eliminar() {
