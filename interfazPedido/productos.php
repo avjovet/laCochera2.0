e3ffad55ce04da4c6665341773ae4420847faf90
Alexandra Jove <93670981+avjovet@users.noreply.github.com>
2024-06-21 22:20:58 -0500

    Corrección de ruta

--- interfazPedido/productos.php
+++ interfazPedido/productos.php
@@ -1,7 +1,7 @@
 <?php
 // Se obtiene un json con todos los productos de la BD
 
-require_once '..\src\controllers\producto_controller.php';
+require_once '../src/controllers/producto_controller.php';
 
 $productoController = new ProductoController();
 
