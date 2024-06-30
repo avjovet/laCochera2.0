b45534527dc3f952f83b423ea6801141f8e4c6c4
LuzKarinaQuispeQuispe <156692087+LuzKarinaQuispeQuispe@users.noreply.github.com>
2024-06-29 23:46:03 -0500

    Actualizar get_deliver_orders_count.php

--- InterfazMesero/get_deliver_orders_count.php
+++ InterfazMesero/get_deliver_orders_count.php
@@ -3,7 +3,15 @@
 include 'conn/conn.php';
 
 // Consulta para obtener la cantidad de pedidos pendientes
-$sql = "SELECT COUNT(*) AS pending_count FROM pedido WHERE Estado = 4";
+$sql = "SELECT COUNT(*) AS pending_count
+FROM pedido p
+WHERE p.Estado = 1
+AND p.Mesa_id IS NOT NULL
+AND EXISTS (
+    SELECT 1
+    FROM detallepedido dp
+    WHERE dp.Pedido_id = p.idPedido
+);";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -13,3 +21,4 @@ echo $row['pending_count'];
 // Cierra la conexión
 $conn = null;
 ?>
+
